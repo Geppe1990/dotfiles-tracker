7fecd4aef82b1d20ba3f8239e8b54aa75c6e9d47
Giuseppe Vigneri <geppe1990@gmail.com>
2024-05-28 09:35:18 +0200

    showLineNumbers handling

--- src/components/CodePreview.tsx
+++ src/components/CodePreview.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Highlight, themes} from "prism-react-renderer"
+import {useSelector} from "react-redux";
+import {RootState} from "../store";
 
 interface CodePreviewProps {
 	content: string;
 }
 
 const CodePreview: React.FC<CodePreviewProps> = ({content}) => {
+	const settings = useSelector((state: RootState) => state.settings);
 	return (
 		<div className="mb-6">
 			<Highlight
@@ -17,7 +20,9 @@ const CodePreview: React.FC<CodePreviewProps> = ({content}) => {
 					<pre style={style}>
 						{tokens.map((line, i) => (
 							<div key={i} {...getLineProps({line})}>
-								{/*<span>{i + 1}</span>*/}
+								{settings.showLineNumbers &&
+									<span>{i + 1}&nbsp;</span>
+								}
 								{line.map((token, key) => (
 									<span key={key} {...getTokenProps({token})} />
 								))}
