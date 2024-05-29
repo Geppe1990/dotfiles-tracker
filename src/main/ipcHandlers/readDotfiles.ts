0a37180963adb0a7000f5f0ad852428bfd3acaa2
Giuseppe Vigneri <geppe1990@gmail.com>
2024-05-29 14:22:46 +0200

    Refactoring files

--- src/main/ipcHandlers/readDotfiles.ts
+++ src/main/ipcHandlers/readDotfiles.ts
@@ -3,17 +3,11 @@ import { ipcMain } from 'electron';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as os from 'os';
+import dotfilesList from '../../files.json'; // Aggiorna il percorso al tuo file JSON
 
 ipcMain.handle('read-dotfiles', async () => {
 	const homedir = os.homedir();
-	const dotfiles = [
-		'.bashrc', '.zshrc', '.vimrc', '.gitconfig', '.profile', '.bash_profile', '.bash_logout',
-		'.inputrc', '.nanorc', '.tmux.conf', '.screenrc', '.config/fish/config.fish',
-		'.config/nvim/init.vim', '.config/nvim/init.lua', '.p10k.zsh', '.aliases', '.functions',
-		'.exports', '.gitignore', '.gitattributes', '.wgetrc', '.curlrc', '.npmrc', '.yarnrc',
-		'.eslintrc', '.prettierrc', '.stylelintrc', '.tern-project', '.jsconfig.json',
-		'.viminfo', '.dircolors', '.cargo/config', '.rustfmt.toml', '.clang-format', '.ackrc'
-	];
+	const dotfiles = dotfilesList.files.map((file: { fileName: string; path: string }) => file.fileName);
 
 	return dotfiles.filter(dotfile => fs.existsSync(path.join(homedir, dotfile)));
 });
